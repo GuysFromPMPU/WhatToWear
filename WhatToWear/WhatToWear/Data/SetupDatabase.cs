7d0507ec0714be34178477bbaf181b4f6e4a8e79
Yury Belousov <yury-belousov@yandex.ru>
2016-10-30 04:13:21 +0300

    add outwear clothes

--- WhatToWear/WhatToWear/Data/SetupDatabase.cs
+++ WhatToWear/WhatToWear/Data/SetupDatabase.cs
@@ -15,7 +15,16 @@ namespace WhatToWear.Data
             {
                 new Clothes {Type = "headwear", Name = "wool-hat", Gender = "unisex", MinTemp = -100, MaxTemp = 5},
                 new Clothes {Type = "headwear", Name = "big-hat", Gender = "male", MinTemp = 5, MaxTemp = 25},
-                new Clothes {Type = "headwear", Name = "woman-hat", Gender = "female", MinTemp = 5, MaxTemp = 30 }
+                new Clothes {Type = "headwear", Name = "woman-hat", Gender = "female", MinTemp = 5, MaxTemp = 30 },
+
+                new Clothes {Type = "outerwear", Name = "shirt-2", Gender = "unisex", MinTemp = 20, MaxTemp = 100},
+                new Clothes {Type = "outerwear", Name = "short-sleeve-t-shirt", Gender = "unisex", MinTemp = 20, MaxTemp = 100},
+                new Clothes {Type = "outerwear", Name = "shirt-5", Gender = "unisex", MinTemp = 15, MaxTemp = 100},
+                new Clothes {Type = "outerwear", Name = "sweater-with-hood", Gender = "male", MinTemp = 10, MaxTemp = 20},
+                new Clothes {Type = "outerwear", Name = "shirt-with-vest", Gender = "male", MinTemp = 10, MaxTemp = 25}
+
+
+
             };
 
             foreach (var clothes in clothesList)
