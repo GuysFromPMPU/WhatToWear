b83b0817534654bf48f92d726dff79187582c419
Yury Belousov <yury-belousov@yandex.ru>
2016-10-30 03:04:45 +0300

    add headwear clothes

--- WhatToWear/WhatToWear/Data/SetupDatabase.cs
+++ WhatToWear/WhatToWear/Data/SetupDatabase.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WhatToWear.Data
 {
-  partial class ClothesDatabase
-  {
-    public void SetupDatabase()
+    partial class ClothesDatabase
     {
-      AddClothes(new Clothes {
-        MinTemp = 20,
-        MaxTemp = 280,
-        Color = "White",
-        Name = "Usual T-shirt Fucking Twice",
-        Type = "T-shirt",
-      });
+        public void SetupDatabase()
+        {
+            var clothesList = new List<Clothes>
+            {
+                new Clothes {Type = "headwear", Name = "wool-hat", Gender = "unisex", MinTemp = -100, MaxTemp = 5},
+                new Clothes {Type = "headwear", Name = "big-hat", Gender = "male", MinTemp = 5, MaxTemp = 25},
+                new Clothes {Type = "headwear", Name = "woman-hat", Gender = "female", MinTemp = 5, MaxTemp = 30 }
+            };
+
+            foreach (var clothes in clothesList)
+            {
+                clothes.Color = "black";
+                AddClothes(clothes);
+            }
+        }
     }
-  }
 }
